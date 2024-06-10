fc7f24c755ef3f9e9b6306205d1d04d153aa73bc
467480903 <467480903@qq.com>
2024-06-10 12:35:35 +0000

    纠正错误

--- rclcpp/topics/minimal_subscriber/member_function.cpp
+++ rclcpp/topics/minimal_subscriber/member_function.cpp
@@ -29,7 +29,7 @@
 #define SHM_KEY 12345
 #define SHM_SIZE (1024 * 1024 * 2)
 
-
+std::vector<std::string> joint_names = {"W1L", "W2R", "W3B", "M1L", "M2L", "M3L", "M4L", "M5L", "M6L", "M1R", "M2R", "M3R", "M4R", "M5R", "M6R"};
 
 using std::placeholders::_1;
 
@@ -41,16 +41,15 @@ public:
   {
 
     subscription_ = this->create_subscription<trajectory_msgs::msg::JointTrajectory>(
-        "/trajectory_controller/joint_trajectory", 10,
+        "/trajectory_controller/joint_trajectory", 1,
         std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
 
-    publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", 10);
+    publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", 1);
+    publisher_2 = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states_target", 1);
 
     timer_ = this->create_wall_timer(
-      std::chrono::milliseconds(500), std::bind(&MinimalSubscriber::timer_callback, this));
+        std::chrono::milliseconds(8), std::bind(&MinimalSubscriber::timer_callback, this));
   }
-
-  
 
 private:
   void topic_callback(const trajectory_msgs::msg::JointTrajectory::SharedPtr msg)
@@ -62,15 +61,23 @@ private:
 
     for (size_t i = 0; i < num_points; ++i)
     {
-      for (size_t j = 0; j < num_joints; ++j)
+      memset(&new_ref, 0, sizeof(new_ref));
+      for (size_t j = 0; j < num_joints; j++)
       {
-        angles[i][j] = msg->points[i].positions[j];
-        
-        memset(&new_ref, 0, sizeof(new_ref));
-        new_ref.motor_ref[j].target_postion = angles[i][j];
-        bool dataOk = edb_push_ref(&new_ref);
-        fprintf(stderr, "dataOk %d \n ", dataOk);
+        for (int i2 = 0; i2 < num_joints; i2++)
+        {
+          std::string name = joint_names[i2];
+          if (name.compare(msg->joint_names[j].c_str()) == 0)
+          {
+            angles[i][i2] = msg->points[i].positions[i2];
+            new_ref.motor_ref[i2].target_postion = angles[i][i2];
+            fprintf(stderr, "a %d : %d , ", j, angles[i][i2]);
+            break;
+          }
+        }
       }
+      bool dataOk = edb_push_ref(&new_ref);
+      fprintf(stderr, "dataOk %d , line: %ld \n ", dataOk, i);
     }
   }
 
@@ -78,35 +85,43 @@ private:
   {
     auto message = sensor_msgs::msg::JointState();
     message.header.stamp = this->now();
-    message.name = {"joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "joint7", "joint8", "joint9", "joint10", "joint11", "joint12", "joint13", "joint14", "joint15", "joint16", "joint17", "joint18"};
+    message.name = joint_names;
+    message.name = {"joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "joint7", "joint8", "joint9", "joint10", "joint11", "joint12", "joint13", "joint14", "joint15"};
     message.position.resize(message.name.size());
     message.effort.resize(message.name.size());
     message.velocity.resize(message.name.size());
 
+    auto message2 = sensor_msgs::msg::JointState();
+    message2.header.stamp = this->now();
+    message2.name = joint_names;
+    message2.name = {"joint1", "joint2", "joint3", "joint4", "joint5", "joint6", "joint7", "joint8", "joint9", "joint10", "joint11", "joint12", "joint13", "joint14", "joint15"};
+    message2.position.resize(message.name.size());
+
     memset(&feedback, 0, sizeof(feedback));
     bool dataOk = edb_pull_fdbk(&feedback);
 
-    if(dataOk){
-      for (size_t i = 0; i < message.name.size(); ++i) {
+    if (dataOk)
+    {
+      for (size_t i = 0; i < message.name.size(); ++i)
+      {
         message.position[i] = feedback.motor_fdbk[i].feedbk_postion;
         message.effort[i] = feedback.motor_fdbk[i].feedbk_torque;
         message.velocity[i] = feedback.motor_fdbk[i].feedbk_speed;
-        
-      }
 
-      
+        message2.position[i] = feedback.motor_fdbk[i].target_position;
+      }
     }
     publisher_->publish(message);
+    publisher_2->publish(message2);
   }
 
-  rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr subscription_ ;
+  rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr subscription_;
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher_2;
   rclcpp::TimerBase::SharedPtr timer_;
   GROUP_REFERENCE new_ref;
   GROUP_FEEDBACK feedback;
-  
 };
-
 
 int main(int argc, char *argv[])
 {
@@ -127,7 +142,6 @@ int main(int argc, char *argv[])
   }
 
   edb_init(appPtr, SHM_SIZE, true);
- 
 
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());
