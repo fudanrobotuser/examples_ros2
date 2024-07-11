b68d61943d6875353ac6b9531993b9751a7299ac
Your Name <you@example.com>
2024-07-11 15:45:46 +0800

    vvvvv

---
+++ src/examples/rclcpp/topics/minimal_subscriber/build/CMakeFiles/subscriber_member_function.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/subscriber_member_function.dir/ecat_data_buffer.c.o"
+  "CMakeFiles/subscriber_member_function.dir/ecat_data_buffer.c.o.d"
+  "CMakeFiles/subscriber_member_function.dir/member_function.cpp.o"
+  "CMakeFiles/subscriber_member_function.dir/member_function.cpp.o.d"
+  "subscriber_member_function"
+  "subscriber_member_function.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/subscriber_member_function.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
